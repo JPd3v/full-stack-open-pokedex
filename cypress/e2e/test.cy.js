d47990c83c5926c207f6b59c8845256cd452e13a
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-06-01 08:19:55 -0300

    adds new e2e test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,7 +1,15 @@
-describe('Pokedex', function () {
-  it('front page can be opened', function () {
-    cy.visit('http://localhost:8080')
-    cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-  })
-})
+/* eslint-disable semi */
+/* eslint-disable quotes */
+describe("Pokedex", function () {
+  it("front page can be opened", function () {
+    cy.visit("http://localhost:8080");
+    cy.contains("ivysaur");
+    cy.contains("Pokémon and Pokémon character names are trademarks of Nintendo.");
+  });
+
+  it("navigates correctly", function () {
+    cy.visit("http://localhost:8080");
+    cy.contains("ivysaur").click();
+    cy.contains("chlorophyll");
+  });
+});
